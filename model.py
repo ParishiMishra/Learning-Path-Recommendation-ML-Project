f333633465dd5989713f911bfce71fc7acc20d00
Parishi <parishi.mishra@gmail.com>
2024-07-16 11:24:27 +0530

    model created

---
+++ model.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import Sequential
+from keras import Dense, InputLayer
+
+X = df[['quiz_scores', 'assignment_completion', 'time_spent_on_tasks', 'frequency_of_interactions', 'emotional_state']].values
+y = df['average_performance'].values
+
+from sklearn.preprocessing import MinMaxScaler
+scaler = MinMaxScaler()
+X_scaled = scaler.fit_transform(X)
+
+# Build the deep reinforcement learning model
+model = Sequential()
+model.add(InputLayer(input_shape=(X_scaled.shape[1],)))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(1, activation='linear'))
+
+model.compile(optimizer='adam', loss='mse')
+model.summary()
+
+# Train the model
+model.fit(X_scaled, y, epochs=50, batch_size=10, validation_split=0.2)
